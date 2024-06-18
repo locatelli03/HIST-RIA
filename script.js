7043ab3288f2d757a49d7d9fd7de03c8b2bd2a8e
locatelli03 <106330744+locatelli03@users.noreply.github.com>
2024-06-18 10:55:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Um belo dia dona Joana estva fazendo sua caminhada quando encontrou um gatinho que miava muito, vendo ele nessas condições pensei em levá-lo para casa ou levar ele para um abrigo de animais. dona Joana levou ele para onde?",
+        alternativas: [
+            "sua casa",
+            "abrigo" ,
+        ]
+    },
+    {
+        enunciado: "Dona Joana escolheu levar ele para casa pra poder presentear seu netinho, mas antes ela tinha que dar um banho nele para entregar ele limpinho. Dona Joana deu banho em casa ou levou para o petShop?",
+        alternativas: [
+            "banho em casa",
+            "banho no petShop"
+        ]
+    }
+
+    {
+        enunciado: "quando Dona Joana foi buscar o gatinho Miau no petShop avistou várias roupinhas para o gato, mas ficou em dúvida entre duas cores de roupa, azul e verde. Qual cor Dona Joana escolheu?",
+        alternativas: [
+            "azul",
+            "verde",
+
+            = [
+                {
+                    enunciado: "quando já estava saindo da loja avistou um brinquedo diferente e interesante para o gatinho, mas ficou em dúvida se comprava ou não",
+                    alternativas: [
+                        "comprou o brinquedo",
+                        "não comprou o brinquedo",           
+               ]
+    },
+    {
+        enunciado: "após ela ter escolhido comprr o brinquedo foi direto para casa levar o gato para seu netinho, ele amou o presente e ficou encantado",
+        
+        
+    
